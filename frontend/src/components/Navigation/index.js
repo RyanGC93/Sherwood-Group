d93918ae34c3cddc70d14afb48657f96500b3dfc
Ryan Conk <ryangconk@gmail.com>
2021-01-26 18:14:40 -0700

    update package.json

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,10 +1,12 @@
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-
+import { ReactSVG } from 'react-svg'
+import Logo from '../../assests/icon.svg'
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import SignupFormModal from '../SignupFormModal';
 import './Navigation.css';
+
 
 import SearchBar from './Search'
 
@@ -17,6 +19,7 @@ const Navigation = ({ isLoaded }) => {
   } else {
     sessionLinks = (
       <li className="navbar__nav-links">
+
         <ul className="auth-btns">
           <li className="navbar__nav-links--navlink">
             <LoginFormModal />
@@ -35,6 +38,10 @@ const Navigation = ({ isLoaded }) => {
         <li className="navbar__nav-links--navlink">
           <NavLink to="/" exact>Home</NavLink>
         </li>
+        {/* <Logo /> */}
+        <ReactSVG src="{Logo}" />
+        <img src={Logo} width={25} />
+        
         <SearchBar />
         <a href="/">Free Stocks</a>
         <a href="/">PortFolio</a>
