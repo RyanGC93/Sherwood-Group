a2dfdb230e1afad1bf0b8360f0f6eab485598d30
Ryan Conk <ryangconk@gmail.com>
2021-04-01 22:20:01 -0600

    add portfolio icons

---
+++ frontend/src/components/LoginSignupPage/PortfolioIcons/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./styles.css"; gs
+
+import {FaLinkedin} from 'react-icons/fa'
+import {FaGithubSquare} from 'react-icons/fa'
+import {SiGmail} from 'react-icons/si'
+
+const PortfolioIcons=() => {
+  
+
+
+  return (
+    <div>
+
+      <div className="professional-header">Professional Resources</div>
+        <div className="professional-row" >
+          <FaLinkedin className='professional-fa' onClick={() => window.location.href = 'https://www.linkedin.com/in/ryan-conk-8084b4103/'}/>
+        <FaGithubSquare className='professional-fa ' onClick={() => window.location.href = 'https://github.com/RyanGC93'} />
+        {/* TODO redirect to email */}
+          <SiGmail className='professional-fa' onClick={() => window.location.href = ''} />
+
+      </div>
+    </div>
+  )
+}
+
+export default PortfolioIcons
